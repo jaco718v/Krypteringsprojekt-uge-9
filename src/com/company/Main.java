dc2e2d46f25100c3f44811293ae2fca05511f9a1
jaco718v <jaco718v@stud.kea.dk>
2022-03-02 10:45:35 +0100

    Added cypherEncrypt-metode fra prototype.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,40 @@
 package com.company;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
 
+    public StringBuilder caesarCypherDecrypt(String inputString, int cypherTal){
+       cypherTal=cypherTal*(-1);
+       return caesarCypherEncrypt(inputString,cypherTal);
+    }
+
+    public char caesarCypherChar(int cypherShift, char letter) {    //Virker ikke ved shift højere end 26
+        char encryptet = letter;
+        if (letter != ' ') {
+            char encryption = (char) (letter + cypherShift);
+            if (encryption > 'Z') {
+                encryption = (char) (encryption - 26);
+            } else if (encryption < 'A') {
+                encryption = (char) (encryption + 26);
+            }
+            encryptet = (encryption);
+        }
+        return encryptet;
+    }
+
+    public StringBuilder caesarCypherEncrypt(String inputString, int cypherTal){
+        StringBuilder text = new StringBuilder();
+        inputString = inputString.toUpperCase(Locale.ROOT);
+        for (int i =0;i<inputString.length();i++ ){
+            text.append(caesarCypherChar(cypherTal,inputString.charAt(i)));
+        }
+        return text;
+    }
 
     public static void main(String[] args) {
+        StringBuilder text=null;
         Scanner sc = new Scanner(System.in);
         Main obj = new Main();
         System.out.println("This program can encrypt and decrypt messages with the Caesar-Cypher," +
@@ -13,7 +42,7 @@ public class Main {
         System.out.println("It will also be possible to repeatably encrypt a message through multiple " +
             "cyphers.");
         System.out.println("Now choose your cypher!");
-        int userChoice=0;
+        int userChoice=1;
         while(userChoice==1 || userChoice==2 || userChoice==3 || userChoice==4) {
             System.out.println("Type the number corresponding to your desired action.\n" +
                 "Your options are:\nCaesar-cypher:\t\tencrypt(1)\tdecrypt(2)");
@@ -21,16 +50,16 @@ public class Main {
             System.out.println("To exit the program enter any other key.");
             userChoice = sc.nextInt();
             switch (userChoice) {
-                case 1 -> System.out.println("Type your message followed by your shift-number");
-                obj.caesarCypherEncrypt(sc.next(), sc.nextInt());
-                case 2 -> System.out.println("Type the encrypted message followed by the shift-number");
-                obj.caesarCypherdecrypt(sc.next(), sc.nextInt());
-                case 3 -> System.out.println("Type your message followed by key of letters");
-                obj.vigenereCypherEncrypt(sc.next(), sc.next());
-                case 4 -> System.out.println("Type the encrypted message followed by the key of letters");
-                obj.vigenereCypherDecrypt(sc.next(), sc.next());
+                case 1 -> {System.out.println("Type your message followed by your shift-number");
+                text = obj.caesarCypherEncrypt(sc.next(), sc.nextInt());}
+                case 2 -> {System.out.println("Type the encrypted message followed by the shift-number");
+                text = obj.caesarCypherDecrypt(sc.next(), sc.nextInt());}
+                case 3 -> {System.out.println("Type your message followed by key of letters");
+                text = obj.vigenereCypherEncrypt(sc.next(), sc.next());}
+                case 4 -> {System.out.println("Type the encrypted message followed by the key of letters");
+                text = obj.vigenereCypherDecrypt(sc.next(), sc.next());}
             }
-
+            System.out.println("You currrent result is: "+text);
         }
     }
 }
